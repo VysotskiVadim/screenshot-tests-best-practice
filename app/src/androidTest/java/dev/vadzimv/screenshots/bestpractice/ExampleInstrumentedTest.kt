bb513ff658fe79287418bcaadff685180fdda7ee
vadzimv <vysotski.vadim@gmail.com>
2021-01-10 16:45:11 +0300

    use not broken version

--- app/src/androidTest/java/dev/vadzimv/screenshots/bestpractice/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/dev/vadzimv/screenshots/bestpractice/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package dev.vadzimv.screenshots.bestpractice
+import dev.vadzimv.screenshots.bestpractice.ScrollingActivity
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
